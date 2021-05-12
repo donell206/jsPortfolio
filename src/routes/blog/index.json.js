db5be5d2c9a20562ff7c26b8f5f1a38173525dc6
Donnel <donnel206@hotmail.com>
2021-05-12 19:41:29 +0200

    commit on origin

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,5 +1,13 @@
 import posts from './_posts.js';
 
+import posts from './_data.js';
+
+export function get(req, res,next) {
+	res.end(JSON.stringify(jobs));
+}
+
+
+
 const contents = JSON.stringify(posts.map(post => {
 	return {
 		title: post.title,
